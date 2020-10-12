d6c832dc23bbda13e0ce3793bfe50d03dfa3f4ac
christeluwgb <christel@uwgb.edu>
2020-10-12 20:27:03 +0000

    Clean up and comments

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@
 
 using namespace std;
 
-// base class used by classes that do OBSERVING!
+// base class used by classes that do OBSERVING (i.e., a "display")!
 class Observer {
   public:
-    virtual void update(int runs, int hits,
-                      float win_average)=0;
+    virtual void update(int runs, int hits, float win_average)=0;
 };
 
 class Subject {
@@ -27,34 +26,47 @@ class SportsData:public Subject {
     float win_average;
     vector<Observer*> observerList;
 
-    public:
-      SportsData() {
-          srand(time(0));  // Initialize random number generator.
-      }
-
-      void registerObserver(Observer* o) {
-        observerList.push_back(o);
-      }
-
-      void unregisterObserver(Observer* o) {
-        for (int i = 0; i < observerList.size(); i++ ) {
-          if (observerList.at(i) == o) {
-            observerList.erase(observerList.begin()+i);
-          }
+    void notifyObservers() {
+      for (int i = 0; i < observerList.size(); i++ ) {
+        if (observerList.at(i) != nullptr) {
+          observerList.at(i)->update(runs,hits,win_average);
         }
       }
+    }
+    
+    int getLatestRuns() {
+      int value = rand() % 10;
+      return (value > hits) ? hits : value;
+    }
 
-      void notifyObservers() {
-        for (int i = 0; i < observerList.size(); i++ ) {
-          if (observerList.at(i) != nullptr) {
-            observerList.at(i)->update(runs,hits,win_average);
-          }
+    int getLatestHits() {
+      return (rand() % 10);
+    }
+
+    float getLatestWinAverage() {
+      return floorf(((float) rand() / (RAND_MAX)) * 1000) / 1000;
+    }
+
+  public:
+    SportsData() {
+        srand(time(0));  // Initialize random number generator.
+    }
+
+    void registerObserver(Observer* o) {
+      observerList.push_back(o);
+    }
+
+    void unregisterObserver(Observer* o) {
+      for (int i = 0; i < observerList.size(); i++ ) {
+        if (observerList.at(i) == o) {
+          observerList.erase(observerList.begin()+i);
         }
       }
+    }
 
-    // This method is used update displays
-    // when data changes
+    // This method is used update data and displays
     void dataChanged() {
+      
       //get latest data
       hits = getLatestHits();
       runs = getLatestRuns();
@@ -63,22 +75,6 @@ class SportsData:public Subject {
       notifyObservers();
     }
 
-    private:
-      int getLatestRuns() {
-        int value = rand() % 10;
-        return (value > hits) ? hits : value;
-    }
-
-
-    int getLatestHits() {
-        return (rand() % 10);
-    }
-
-    float getLatestWinAverage() {
-      return floorf(((float) rand() / (RAND_MAX)) * 1000) / 1000;
-    }
-
-
 };
 
 class CalcScoreDisplay: public Observer {
@@ -86,15 +82,17 @@ class CalcScoreDisplay: public Observer {
     float runRate;
 
   public:
-    void update(int runs, int hits,
-                       float win_average) {
+    void update(int runs, int hits, float win_average) {
                          
-      runRate = (hits > 0) ? (ceilf(((float)runs/(float)hits)*1000)/1000) : 0;
+      runRate = (hits > 0)
+       ? (ceilf(((float)runs /  (float)hits)*1000)/1000) 
+       : 0;
       display();
     }
 
     void display() {
-      cout << "\nRuns Per Hit: \n" << runRate << "\n";
+      cout << "\nRuns Per Hit: \n";
+      cout << runRate << "\n";
     }
 };
 
@@ -113,33 +111,27 @@ class CurrentScoreDisplay: public Observer {
     }
 
     void display() {
-      cout << "\nCurrent Score Display:\n"
-                           << "Runs: " << runs <<
-                           "\nHits:" << hits <<
-                           "\nWin Average: " << win_average << "\n";
+      cout << "\nCurrent Score Display:\n";
+      cout << "Runs: " << runs;
+      cout << "\nHits:" << hits;
+      cout << "\nWin Average: " << win_average << "\n";
     }
 };
 
 int main() {
-        // create objects for testing
-        CalcScoreDisplay calcScoreDisplay = CalcScoreDisplay();
-        CurrentScoreDisplay currentScoreDisplay = CurrentScoreDisplay();
+  // create objects for display
+  CalcScoreDisplay calcScoreDisplay = CalcScoreDisplay();
+  CurrentScoreDisplay currentScoreDisplay = CurrentScoreDisplay();
 
-        // pass the displays to Cricket data
-        SportsData sportsData = SportsData();
+  SportsData sportsData = SportsData();
 
-        // register display elements
-        sportsData.registerObserver(&calcScoreDisplay);
-        sportsData.registerObserver(&currentScoreDisplay);
+  // register display elements
+  sportsData.registerObserver(&calcScoreDisplay);
+  sportsData.registerObserver(&currentScoreDisplay);
 
-        // in real app you would have some logic to
-        // call this function when data changes
-        sportsData.dataChanged();
+  sportsData.dataChanged();
 
-        //remove an observer
-        sportsData.unregisterObserver(&currentScoreDisplay);
-
-        // now only currentScoreDisplay gets the
-        // notification
-        sportsData.dataChanged();
-    } 
+  //remove an observer and re-run dataChanged()
+  sportsData.unregisterObserver(&currentScoreDisplay);
+  sportsData.dataChanged();
+} 
